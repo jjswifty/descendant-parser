7c09c324c0ef31f2adca6ffa71677bb07b9128fc
Эдмон Нарметов <jjswift.dev@gmail.com>
2023-05-27 00:32:40 +0300

    test(*): better semantic

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -3,43 +3,49 @@ import { Parser } from '../src';
 import { unwrapString } from './utils/unwrap-string';
 
 describe('Parser', () => {
-    it('should parse number correctly', () => {
-        const parser = new Parser();
+    describe('NumberLiteral', () => {
+        it('should parse correctly', () => {
+            const parser = new Parser();
 
-        const simpleProgram = '42';
+            const simpleProgram = '42';
 
-        const ast = parser.parse(simpleProgram);
+            const ast = parser.parse(simpleProgram);
 
-        expect(ast).toStrictEqual({
-            type: 'Program',
-            body: {
-                type: 'NumericLiteral',
-                value: 42,
-            },
+            expect(ast).toStrictEqual({
+                type: 'Program',
+                body: {
+                    type: 'NumericLiteral',
+                    value: 42,
+                },
+            });
         });
     });
 
-    it('should parse string correctly', () => {
-        const parser = new Parser();
+    describe('StringLiteral', () => {
+        it('should parse string correctly', () => {
+            const parser = new Parser();
 
-        const simpleProgram = '"hi"';
+            const simpleProgram = '"hi"';
 
-        const ast = parser.parse(simpleProgram);
+            const ast = parser.parse(simpleProgram);
 
-        expect(ast).toStrictEqual({
-            type: 'Program',
-            body: {
-                type: 'StringLiteral',
-                value: unwrapString(simpleProgram),
-            },
+            expect(ast).toStrictEqual({
+                type: 'Program',
+                body: {
+                    type: 'StringLiteral',
+                    value: unwrapString(simpleProgram),
+                },
+            });
         });
     });
 
-    it('should fail by the end of input', () => {
-        const parser = new Parser();
+    describe('Errors', () => {
+        it('should fail by the end of input', () => {
+            const parser = new Parser();
 
-        const program = '';
+            const program = '';
 
-        expect(() => parser.parse(program)).toThrowError('Unsupported token type.');
+            expect(() => parser.parse(program)).toThrowError('Unsupported token type.');
+        });
     });
 });
