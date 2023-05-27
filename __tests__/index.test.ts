34096407ad23ba796d18db225befaf634b3d9ca5
Эдмон Нарметов <jjswift.dev@gmail.com>
2023-05-27 14:05:13 +0300

    test(tests): add tests for whitespaces, error handling

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -4,7 +4,7 @@ import { unwrapString } from './utils/unwrap-string';
 
 describe('Parser', () => {
     describe('NumberLiteral', () => {
-        it('should parse correctly', () => {
+        it('should parse number correctly', () => {
             const parser = new Parser();
 
             const simpleProgram = '42';
@@ -39,6 +39,42 @@ describe('Parser', () => {
         });
     });
 
+    describe('Ignore', () => {
+        describe('whitespaces', () => {
+            it('should ignore whitespaces around number', () => {
+                const parser = new Parser();
+
+                const program = '      1     ';
+
+                const ast = parser.parse(program);
+
+                expect(ast).toStrictEqual({
+                    type: 'Program',
+                    body: {
+                        type: 'NumericLiteral',
+                        value: 1,
+                    },
+                });
+            });
+
+            it('should ignore whitespaces around string', () => {
+                const parser = new Parser();
+
+                const program = '      "hi"     ';
+
+                const ast = parser.parse(program);
+
+                expect(ast).toStrictEqual({
+                    type: 'Program',
+                    body: {
+                        type: 'StringLiteral',
+                        value: 'hi',
+                    },
+                });
+            });
+        });
+    });
+
     describe('Errors', () => {
         it('should fail by the end of input', () => {
             const parser = new Parser();
@@ -46,6 +82,38 @@ describe('Parser', () => {
             const program = '';
 
             expect(() => parser.parse(program)).toThrowError('Unsupported token type.');
+        });
+
+        it('should fail by the end despite whitespace', () => {
+            const parser = new Parser();
+
+            const program = '    ';
+
+            expect(() => parser.parse(program)).toThrowError('Unsupported token type.');
+        });
+
+        it('should fail if string does not have quote from backside', () => {
+            const parser = new Parser();
+
+            const program = '"Hi ';
+
+            expect(() => parser.parse(program)).toThrowError('Unexpected token: "');
+        });
+
+        it('should fail if string does not have quote from frontside', () => {
+            const parser = new Parser();
+
+            const program = 'Hi"';
+
+            expect(() => parser.parse(program)).toThrowError('Unexpected token: H');
+        });
+
+        it('should fail if string does not have quote from frontside despite whitespace', () => {
+            const parser = new Parser();
+
+            const program = '    Hi"';
+
+            expect(() => parser.parse(program)).toThrowError('Unexpected token: H');
         });
     });
 });
